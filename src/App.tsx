21a95bbc801d8d1a16381da0184071d5abe491c8
P4CM4N890 <p4cm4n890@gmail.com>
2023-07-16 22:08:31 -0600

    Pruebas para obtener dimensiones

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,26 @@ import { DVDIcon } from './components/DVDIcon';
 
 function App() {
 
+  // const [ top, setTop ] = useState( 0 );
+  // const [ left, setLeft ] = useState( 0 );
+
+  const getDimensions = () => {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+  }
+
+  const [ screenSize, setScreenSize ] = useState( getDimensions() );
+
+  useEffect(() => {
+    // setTimeout( () => {
+    //   move();
+    // }, 10 );
+    setScreenSize( getDimensions() );
+  }, [ screenSize ]);
+  
+  
   return (
     <div className='App'>
         <DVDIcon width='22vw' color='#60D833' />
