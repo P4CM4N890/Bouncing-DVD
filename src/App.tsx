7bc69b97ff47178aea03ba7c62a57c07bac788dc
P4CM4N890 <p4cm4n890@gmail.com>
2023-07-18 00:03:32 -0600

    Function to get Window size

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,66 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { DVDIcon } from './components/DVDIcon';
 
+interface Size {
+  width: number,
+  height: number,
+}
+
+interface Collision {
+  right: number,
+  bottom: number,
+}
+
 function App() {
 
-  // const [ top, setTop ] = useState( 0 );
-  // const [ left, setLeft ] = useState( 0 );
+  const [ top, setTop ] = useState( 10 );
+  const [ left, setLeft ] = useState( 10 );
+
+  const [ size, setSize ] = useState< Size >({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const [ collision, setCollision ] = useState< Collision >({
+    right: window.innerWidth - 200,
+    bottom: window.innerHeight - 90,
+  });
 
   const getDimensions = () => {
-    return {
-      width: window.innerWidth,
-      height: window.innerHeight
-    }
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    setSize({
+      width: width,
+      height: height,
+    });
+    
+    setCollision({
+      right: width - 200,
+      bottom: height - 90,
+    });
   }
 
-  const [ screenSize, setScreenSize ] = useState( getDimensions() );
-
   useEffect(() => {
-    // setTimeout( () => {
-    //   move();
-    // }, 10 );
-    setScreenSize( getDimensions() );
-  }, [ screenSize ]);
+      // Event listener when the window is resize
+    window.addEventListener( 'resize', getDimensions );
+
+      // Remove the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener( 'resize', getDimensions );
+    }
+  }, []);
   
   
   return (
     <div className='App'>
-        <DVDIcon width='22vw' color='#60D833' />
+        <DVDIcon 
+          width='200px' 
+          color='#60D833' 
+          top={ top }
+          left={ left }
+        />
+        <p style={{ color: 'white' }}>{ size?.width }</p>
+        <p style={{ color: 'white' }}>{ collision?.right }</p>
     </div>
   );
 }
